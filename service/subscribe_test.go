b69910f27a35b70c761ecfe140f5e87ddef1389c
Soulou <leo@unbekandt.eu>
2014-02-08 23:01:15 +0000

    Add sleep to avoid Register to be executed before Watch

--- service/subscribe_test.go
+++ service/subscribe_test.go
@@ -3,19 +3,21 @@ package service
 import (
 	. "github.com/smartystreets/goconvey/convey"
 	"testing"
+	"time"
 )
 
 // Tests
 func TestSubscribe(t *testing.T) {
 	Convey("When we subscribe a service, we get all the notifications from it", t, func() {
 		responses := Subscribe("test_subs")
+		time.Sleep(200 * time.Millisecond)
 		Convey("When something happens about this service, the responses must be gathered in the channel", func() {
 			_, err := client.Create("/services/test_subs/key", "test", 0)
 			So(err, ShouldBeNil)
 			r := <-responses
 			So(r, ShouldNotBeNil)
 			So(r.Node.Key, ShouldEqual, "/services/test_subs/key")
-			So(r.Action, ShouldEqual,"create")
+			So(r.Action, ShouldEqual, "create")
 			_, err = client.Delete("/services/test_subs/key", false)
 			So(err, ShouldBeNil)
 			r = <-responses
@@ -47,6 +49,7 @@ func TestSubscribeNew(t *testing.T) {
 
 	Convey("When the service 'test' is watched and a host registered", t, func() {
 		hosts := SubscribeNew("test_new")
+		time.Sleep(200 * time.Millisecond)
 		Register("test_new", genHost(), stop)
 		Convey("A host should be available in the channel", func() {
 			host, ok := <-hosts
