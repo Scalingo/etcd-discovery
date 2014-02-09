3df1973c012a0d725b59a6cffeafdd83cdc0ddc0
Soulou <leo@unbekandt.eu>
2014-02-09 11:30:57 +0000

    Get return empty array when there is no host/no such service

--- service/get.go
+++ service/get.go
@@ -1,8 +1,16 @@
 package service
 
+import (
+	"github.com/coreos/go-etcd/etcd"
+)
+
 func Get(service string) ([]*Host, error) {
 	res, err := client.Get("/services/" + service, false, true)
 	if err != nil {
+		// If the service does not exist
+		if etcdErr, ok := err.(*etcd.EtcdError); ok && etcdErr.ErrorCode == 100 {
+			return []*Host{}, nil
+		}
 		return nil, err
 	}
 
