44de547317ecb78e4176631c7726f14082db7c48
Soulou <leo@unbekandt.eu>
2014-04-06 21:08:35 +0100

    Create TLS authentication if variables from environment are set

--- service/init.go
+++ service/init.go
@@ -1,15 +1,21 @@
 package service
 
 import (
-	"github.com/coreos/go-etcd/etcd"
+	"io/ioutils"
 	"log"
 	"os"
+
+	"github.com/coreos/go-etcd/etcd"
 )
 
 var (
 	logger   *log.Logger
 	client   *etcd.Client
 	hostname string
+
+	cacert  = os.Getenv("ETCD_CACERT")
+	tlskey  = os.Getenv("ETCD_TLS_KEY")
+	tlscert = os.Getenv("ETCD_TLS_CERT")
 )
 
 func init() {
@@ -17,7 +23,11 @@ func init() {
 	if len(os.Getenv("ETCD_HOST")) != 0 {
 		host = os.Getenv("ETCD_HOST")
 	}
-	client = etcd.NewClient([]string{host})
+	if len(cacert) != 0 && len(tlskey) != 0 && len(tlscert) != 0 {
+		client = newTLSClient([]string{host})
+	} else {
+		client = etcd.NewClient([]string{host})
+	}
 
 	if len(os.Getenv("HOSTNAME")) != 0 {
 		hostname = os.Getenv("HOSTNAME")
@@ -30,4 +40,24 @@ func init() {
 	}
 
 	logger = log.New(os.Stderr, "[etcd-discovery]", log.LstdFlags)
+}
+
+func newTLSClient(hosts []string) *etcd.Client {
+	cacertContent, err := ioutils.ReadAll(cacert)
+	if err != nil {
+		panic(err)
+	}
+	keyContent, err := ioutils.ReadAll(tlskey)
+	if err != nil {
+		panic(err)
+	}
+	certContent, err := ioutils.ReadAll(tlscert)
+	if err != nil {
+		panic(err)
+	}
+	c, err := etcd.NewTLSClient(hosts, string(certContent), string(keyContent), string(cacertContent))
+	if err != nil {
+		panic(err)
+	}
+	return c
 }
